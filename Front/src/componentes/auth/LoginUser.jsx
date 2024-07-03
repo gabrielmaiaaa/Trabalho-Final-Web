bc9a9124831ac1ed320f8dcbfe2d30f1c91ea7c7
Maia <gabrielmaia6743@gmail.com>
2024-07-03 16:23:37 -0300

    Alguns .jsx do ambiente

---
+++ Front/src/componentes/auth/LoginUser.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function LoginUser() {
+    
+  return (
+    <p>asdasd</p>
+  )
+}
--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import CreateUser from './componentes/auth/CreateUser.jsx'
+import LoginUser from './componentes/auth/LoginUser.jsx'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <LoginUser />
+      },
+      {
+        path: 'criar-user',
+        element: <CreateUser />
+      }
+    ]
+  }
+])
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={routes}/>
 )
